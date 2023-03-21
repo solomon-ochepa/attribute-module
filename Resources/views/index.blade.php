cd58330c5c7ba16ea146b7c2a4f5720dd0c02c7e
Solomon Ochepa <solomonochepa@gmail.com>
2023-03-21 14:43:45 +0100

    Initial commit

---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('attribute::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('attribute.name') !!}
+    </p>
+@endsection
